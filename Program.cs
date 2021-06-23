65e61b53158a78fa0c09243cd7b4173c5d667ec5
Minhal733 <57898271+Minhal733@users.noreply.github.com>
2021-06-23 04:45:53 -0700

    Add files via upload
    This code is written using C#, and performs google search automatically. Program.cs is the source code file.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        string actual ;
+        string expected = "Google";
+        static void Main(string[] args)
+        {
+            
+        }
+        [SetUp]
+        public void Inialize()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+        }
+        [Test]
+        public void Execute()
+        {
+            //find the element (search bar)
+            IWebElement searchbox = driver.FindElement(By.Name("q"));
+            // write in search bar
+            searchbox.SendKeys("facebook");
+
+            actual = driver.Title;
+            Assert.AreEqual(actual, expected);
+
+            IWebElement search = driver.FindElement(By.ClassName("gNO89b"));
+            search.Click();
+        }
+        [TearDown]
+        public void EndTest()
+        {
+            //close browser
+            //driver.Quit();
+        }
+    }
+}
